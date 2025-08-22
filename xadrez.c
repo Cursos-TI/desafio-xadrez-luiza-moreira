8919a85131ebab67b1c2d2115aad28af186a36b2
luiza-moreira <luizaferreira75@gmail.com>
2025-08-22 00:25:04 +0000

    Movimentação das peças bispo, torre e rainha.

--- xadrez.c
+++ xadrez.c
@@ -7,16 +7,30 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
+    int b, t, r;
+    t = 0;
+    r = 0;
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
+    
+    for (b=0; b<5 ; b++){
+        printf("Cima. Direita. \n");
+    
+    }
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
+    
+    while (t<5){
+        printf("Direita.\n");
+        t++;
+    }
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+    
+    do {
+        printf("Esquerda.\n");
+        r++;
+    } while (r<8);
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
